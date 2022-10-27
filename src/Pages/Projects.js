f173123523fd446cf0e2690623ce681e5e13b7ea
Rdoolz51 <Rdoolz51@aol.com>
2022-10-27 14:24:27 -0400

    added links

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -14,9 +14,9 @@ const Projects = () => {
                 <h1 className="m-0 mt-5">These projects demonstrate my journey from working with basic html and css to creating full stack webpages</h1>
             </div>
             <div className="container d-flex p-5">
-                <Project img={dndLandingPage} title="D&D Character Creation Tool" description="A tool that creates a randomized character for you. You can also input specfic details about your character if you want to as well" />
-                <Project img={boardgametrackerss} title="Board Game Tracker" description="Allows the user to make a list of board games from a database. The list can be filtered by category, name, number of players, time to play, etc..." />
-                <Project img={choreboardss} title="Chore-Board" description="Chore Board is a website that allows parents to post a list of chores. The children listed on the account can complete chores to earn points to spend on custom rewards!" />
+                <Project img={dndLandingPage} title="D&D Character Creation Tool" description="A tool that creates a randomized character for you. You can also input specfic details about your character if you want to as well" link="https://rdoolz51.github.io/DDCharacterCreationPersonal/" />
+                <Project img={boardgametrackerss} title="Board Game Tracker" description="Allows the user to make a list of board games from a database. The list can be filtered by category, name, number of players, time to play, etc..." link="https://board-g4me-tracker.herokuapp.com/" />
+                <Project img={choreboardss} title="Chore-Board" description="Chore Board is a website that allows parents to post a list of chores. The children listed on the account can complete chores to earn points to spend on custom rewards!" link="https://chore-board.herokuapp.com/" />
                 <Project img={placeholderimg} title="Placeholder Name" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." />
             </div>
             <Footer />
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
     return (
         <div className="container-fluid p-0">
-            <footer className="bg-dark bg-opacity-25 text-white d-flex justify-content-center">
+            <footer className="bg-dark  text-white d-flex justify-content-center">
                 <Nav.Link href="https://www.github.com/Rdoolz51" className="fs-3 " style={{ paddingLeft: "2rem" }}>
                     Rdoolz51 <FaGithub />
                 </Nav.Link>
